ab7531c90a3fadced077bf1a1eb070bfb09a474b
ArianeNantes <ariane.salvinien@bbox.fr>
2024-05-02 06:56:44 +0200

    Simple kernel Compute figsize in display importance

--- src/p7_simple_kernel.py
+++ src/p7_simple_kernel.py
@@ -60,7 +60,7 @@ CONFIG_SIMPLE = {
     "generate_submission_files": True,
     "model_dir": MODEL_DIR,
     "model_subdir": "light_simple/",
-    "importance_filename": "feature_importance_in_folds.csv",
+    "importance_filename": "feature_importance.csv",  # Average over NUM_FOLDS
     "submission_filename": "lightgbm_simple_submission.csv",
     "num_threads": NUM_THREADS,
     "stratified_kfold": True,
@@ -474,10 +474,20 @@ def kfold_lightgbm_simple(df=None, config=CONFIG_SIMPLE):
         gc.collect()
     print("Full AUC score %.6f" % roc_auc_score(train_df["TARGET"], oof_preds))
 
+    # Compute average and std importance importance
+    mean_importance = (
+        feature_importance_df.drop("fold", axis=1)
+        .groupby("feature")
+        .agg(
+            importance_mean=("importance", "mean"),
+            importance_std=("importance", "std"),
+        )
+        .sort_values(by="importance_mean", ascending=False)
+    )
     # write importance
     path_model = os.path.join(config["model_dir"], config["model_subdir"])
     importance_filepath = os.path.join(path_model, config["importance_filename"])
-    feature_importance_df.to_csv(importance_filepath, index=False)
+    mean_importance.to_csv(importance_filepath, index=False)
     print("Importance saved in", importance_filepath)
 
     # Write submission file
@@ -487,179 +497,23 @@ def kfold_lightgbm_simple(df=None, config=CONFIG_SIMPLE):
         test_df[["SK_ID_CURR", "TARGET"]].to_csv(submission_filepath, index=False)
         print("Submission saved in", submission_filepath)
     # display_importances(feature_importance_df)
-    return feature_importance_df
-
-
-# Cette fonction provient du kernell full
-"""def kfold_lightgbm_sklearn(data, categorical_feature=None):
-    # Ajout pour error : [LightGBM] [Fatal] Do not support special JSON characters in feature name.
-    data = data.rename(columns=lambda x: re.sub("[^A-Za-z0-9_]+", "", x))
-
-    df = data[data["TARGET"].notnull()]
-    test = data[data["TARGET"].isnull()]
-    print("Train/valid shape: {}, test shape: {}".format(df.shape, test.shape))
-    del_features = [
-        "TARGET",
-        "SK_ID_CURR",
-        "SK_ID_BUREAU",
-        "SK_ID_PREV",
-        "index",
-        "level_0",
-    ]
-    predictors = list(filter(lambda v: v not in del_features, df.columns))
-
-    if not STRATIFIED_KFOLD:
-        folds = KFold(n_splits=NUM_FOLDS, shuffle=True, random_state=RANDOM_SEED)
-    else:
-        folds = StratifiedKFold(
-            n_splits=NUM_FOLDS, shuffle=True, random_state=RANDOM_SEED
-        )
-
-    # Hold oof predictions, test predictions, feature importance and training/valid auc
-    oof_preds = np.zeros(df.shape[0])
-    sub_preds = np.zeros(test.shape[0])
-    importance_df = pd.DataFrame()
-    eval_results = dict()
-
-    for n_fold, (train_idx, valid_idx) in enumerate(
-        folds.split(df[predictors], df["TARGET"])
-    ):
-        train_x, train_y = df[predictors].iloc[train_idx], df["TARGET"].iloc[train_idx]
-        valid_x, valid_y = df[predictors].iloc[valid_idx], df["TARGET"].iloc[valid_idx]
-
-        params = {"random_state": RANDOM_SEED, "nthread": NUM_THREADS}
-        # [TODO : modifier l'appel à la constante globale LIGHTGBM_PARAM]
-        clf = LGBMClassifier(**{**params, **LIGHTGBM_PARAMS_SIMPLE})
-
-        if not categorical_feature:
-            clf.fit(
-                train_x,
-                train_y,
-                eval_set=[(train_x, train_y), (valid_x, valid_y)],
-                eval_metric="auc",
-                # verbose=400,
-                callbacks=[lgb.early_stopping(stopping_rounds=EARLY_STOPPING)],
-            )
-        else:
-            clf.fit(
-                train_x,
-                train_y,
-                eval_set=[(train_x, train_y), (valid_x, valid_y)],
-                eval_metric="auc",
-                # verbose=400,
-                callbacks=[lgb.early_stopping(stopping_rounds=EARLY_STOPPING)],
-                feature_name=list(df[predictors].columns),
-                categorical_feature=categorical_feature,
-            )
-
-        oof_preds[valid_idx] = clf.predict_proba(
-            valid_x, num_iteration=clf.best_iteration_
-        )[:, 1]
-        sub_preds += (
-            clf.predict_proba(test[predictors], num_iteration=clf.best_iteration_)[:, 1]
-            / folds.n_splits
-        )
-
-        # Feature importance by GAIN and SPLIT
-        fold_importance = pd.DataFrame()
-        fold_importance["feature"] = predictors
-        fold_importance["gain"] = clf.booster_.feature_importance(
-            importance_type="gain"
-        )
-        fold_importance["split"] = clf.booster_.feature_importance(
-            importance_type="split"
-        )
-        importance_df = pd.concat([importance_df, fold_importance], axis=0)
-        eval_results["train_{}".format(n_fold + 1)] = clf.evals_result_["training"][
-            "auc"
-        ]
-        eval_results["valid_{}".format(n_fold + 1)] = clf.evals_result_["valid_1"][
-            "auc"
-        ]
-
-        print(
-            "Fold %2d AUC : %.6f"
-            % (n_fold + 1, roc_auc_score(valid_y, oof_preds[valid_idx]))
-        )
-        del clf, train_x, train_y, valid_x, valid_y
-        gc.collect()
-
-    print("Full AUC score %.6f" % roc_auc_score(df["TARGET"], oof_preds))
-    # test["TARGET"] = sub_preds.copy()
-    test.loc[:, ["TARGET"]] = sub_preds.copy()
-
-    # Get the average feature importance between folds
-    mean_importance = importance_df.groupby("feature").mean().reset_index()
-    mean_importance.sort_values(by="gain", ascending=False, inplace=True)
-    # Save feature importance, test predictions and oof predictions as csv
-    if GENERATE_SUBMISSION_FILES:
-
-        # Generate oof csv
-        oof = pd.DataFrame()
-        # oof["SK_ID_CURR"] = df["SK_ID_CURR"].copy()
-        oof.loc[:, ["SK_ID_CURR"]] = df["SK_ID_CURR"].copy()
-        # df["PREDICTIONS"] = oof_preds.copy()
-        df.loc[:, ["PREDICTIONS"]] = oof_preds.copy()
-        # df["TARGET"] = df["TARGET"].copy()
-        df.loc[:, ["TARGET"]] = df["TARGET"].copy()
-        df.to_csv(
-            os.path.join(MODEL_DIR, "oof{}.csv".format(SUBMISSION_SUFIX)), index=False
-        )
-        # Save submission (test data) and feature importance
-        test[["SK_ID_CURR", "TARGET"]].to_csv(
-            os.path.join(MODEL_DIR, "submission{}.csv".format(SUBMISSION_SUFIX)),
-            index=False,
-        )
-        mean_importance.to_csv(
-            os.path.join(
-                MODEL_DIR, "feature_importance{}.csv".format(SUBMISSION_SUFIX)
-            ),
-            index=False,
-        )
-    return mean_importance"""
-
-
-# On a modifié cette fonction car elle trie l'importance sur le meilleur fold et non pas sur l'importance moyenne
-"""def display_importances_old(feature_importance_df_):
-    cols = (
-        feature_importance_df_[["feature", "importance"]]
-        .groupby("feature")
-        .mean()
-        .sort_values(by="importance", ascending=False)[:40]
-        .index
-    )
-    best_features = feature_importance_df_.loc[
-        feature_importance_df_.feature.isin(cols)
-    ]
-    plt.figure(figsize=(8, 10))
-    sns.barplot(
-        x="importance",
-        y="feature",
-        data=best_features.sort_values(by="importance", ascending=False),
-    )
-    plt.title("LightGBM Features (avg over folds)")
-    plt.tight_layout()
-    plt.savefig("lgbm_importances01.png")
-"""
+    return mean_importance
 
 
 # Display/plot feature importance
-def display_importances(feature_importance_in_folds, top=40, sort_by_name=False):
-    mean_importance = (
-        feature_importance_in_folds.drop("fold", axis=1)
-        .groupby("feature")
-        .agg(
-            importance_mean=("importance", "mean"),
-            importance_std=("importance", "std"),
-        )
-        .sort_values(by="importance_mean", ascending=False)
-    )
-
+def display_importances(mean_importance, top=40, sort_by_name=False):
     best_importance = mean_importance.head(top)
     if sort_by_name:
         best_importance = best_importance.sort_values(by="feature")
 
-    fig = plt.figure(figsize=(8, 10))
+    # Taille de la figure
+    width = 8
+    margin = 1
+    bar_height = 0.3
+    height = margin + bar_height * top
+
+    # print(best_importance)
+    fig = plt.figure(figsize=(width, height))
     barplot = sns.barplot(
         x="importance_mean",
         y="feature",
