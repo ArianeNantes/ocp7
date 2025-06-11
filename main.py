1ed8d6df6eac317ad6225bd674c683eeef8d395d
arianewsl <ariane.salvinien@bbox.fr>
2025-06-11 19:11:25 +0200

    Test Hello Azure2

--- main.py
+++ main.py
@@ -4,22 +4,21 @@ from fastapi import FastAPI
 # from src.api.predictor import load_model, load_data_test, load_threshold
 
 app = FastAPI(
-    title="API Prédiction Client",
-    description="API pour prédire un résultat à partir de l'ID client",
-    version="1.0.0",
+    # title="API Prédiction Client",
+    # description="API pour prédire un résultat à partir de l'ID client",
+    # version="1.0.0",
 )
 
 # Charger le modèle et les données au démarrage
 """@app.on_event("startup")
 def startup_event():"""
-"""
-load_model()
-load_threshold()
-load_data_test()
+
+# load_model()
+# load_threshold()
+# load_data_test()
 
 # Ajouter les routes
-app.include_router(prediction_router)
-"""
+# app.include_router(prediction_router)
 
 
 @app.get("/")
