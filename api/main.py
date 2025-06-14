acafd51957eca7af8d8c6ebd79e8ebaa3829bddb
arianewsl <ariane.salvinien@bbox.fr>
2025-06-14 12:58:58 +0200

    deploy only api package

--- api/main.py
+++ api/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+import joblib
+import os
 
-app = FastAPI()
+# from src.api.routes import router as prediction_router
+# from src.api.predictor import load_model, load_data_test, load_threshold
+
+app = FastAPI(
+    # title="API Prédiction Client",
+    # description="API pour prédire un résultat à partir de l'ID client",
+    # version="1.0.0",
+)
+
+
+# load_model()
+# load_threshold()
+# load_data_test()
+
+# Ajouter les routes
+# app.include_router(prediction_router)
 
 
 @app.get("/")
-def read_root():
-    return {"message": "API OK"}
+def root():
+    # Chemin absolu vers le modèle
+    model_path = os.path.join(
+        os.path.dirname(__file__), "models", "best_model_lgbm.pkl"
+    )
+    model = joblib.load(model_path)
+    return {"model_class": model.__class__.__name__}
