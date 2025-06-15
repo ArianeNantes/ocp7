d1b3658ac06648a0b6421938a8ad453590b08c3e
arianewsl <ariane.salvinien@bbox.fr>
2025-06-15 19:30:26 +0200

    load model and threshold

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import joblib
 import os
 
 # from src.api.routes import router as prediction_router
-# from src.api.predictor import load_model, load_data_test, load_threshold
+from src.api.predictor import load_model, load_data_test, load_threshold
 
 app = FastAPI(
     # title="API Prédiction Client",
@@ -12,18 +12,18 @@ app = FastAPI(
 )
 
 
-# load_model()
-# load_threshold()
+model = load_model()
+threshold = load_threshold()
 # load_data_test()
 
 # Ajouter les routes
 # app.include_router(prediction_router)
 
-# Chemin absolu vers le modèle (dans models/ car le dossier api/ est déployé à la racine)
-model_path = os.path.join(os.path.dirname(__file__), "models", "best_model_lgbm.pkl")
-model = joblib.load(model_path)
+# Chemin absolu vers le modèle (/home/site/wwwroot/models/best_model_lgbm.pkl)
+# model_path = os.path.join(os.path.dirname(__file__), "models", "best_model_lgbm.pkl")
+# model = joblib.load(model_path)
 
 
 @app.get("/")
 def root():
-    return {"model_path": model_path, "model_class": model.__class__.__name__}
+    return {"model_class": model.__class__.__name__, "threshold": threshold}
